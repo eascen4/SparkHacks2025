6f84e99f97135b8776d24408425771c3eab06705
sammypatel06 <sammypatel06@gmail.com>
2025-02-08 12:48:59 -0600

    Update _auth.tsx

--- client/src/routes/_auth.tsx
+++ client/src/routes/_auth.tsx
@@ -1,13 +1,4 @@
-import { LoginForm } from '@/components/login-form'
 import { createFileRoute, Outlet } from '@tanstack/react-router'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 
 export const Route = createFileRoute('/_auth')({
   component: RouteComponent,
