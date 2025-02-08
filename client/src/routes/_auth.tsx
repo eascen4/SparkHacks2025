7971a0070f87519e6bd73319bd9f8efb0e88db87
sammypatel06 <sammypatel06@gmail.com>
2025-02-08 10:43:30 -0600

    Added login page
    This is the login page

--- client/src/routes/_auth.tsx
+++ client/src/routes/_auth.tsx
@@ -1,9 +1,22 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { LoginForm } from '@/components/login-form'
+import { createFileRoute, Outlet } from '@tanstack/react-router'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export const Route = createFileRoute('/_auth')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <div>Hello "/_auth"!</div>
+  return  <>
+    <Card style={{display: 'flex',  justifyContent:'center', alignItems:'center', height: '100vh', width: '100vw'}}>
+      <CardContent className="flex items-center space-x-4 rounded-2xl border p-[45px] shadow-lg"><LoginForm></LoginForm></CardContent>
+    </Card>
+  </>
 }
